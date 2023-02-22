87c3a06c198d680a2307ea9bd21de7b3d079a0b4
uwagaDUDE <puvir00@gmail.com>
2023-02-22 11:27:31 +0300

    added csv file, csv load system, max len for item name, test coverage 80%

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ class Item:
     items = []
     item_discount = 0
 
-
     def __init__(self, item_name='', item_price=0, item_value=0, currency=''):
+        """
+        Получение информации о товаре
+        :param item_name: Наименование товара
+        :param item_price: Цена за 1 единицу
+        :param item_value: Количество
+        :param currency: Валюта(любая)
+        """
         self.item_name = item_name
         self.item_price = item_price
         self.item_value = item_value
         self.currency = currency
         Item.items.append(self)
-
+        if self.len_item_name_check(self.item_name):
+            self.len_item_name_error(self.item_name)
     @property
     def name(self):
         return self._name
@@ -19,47 +26,92 @@ class Item:
     @name.setter
     def name(self, value):
         if self.len_item_name_check(value):
-            self.len_item_name_error()
+            self.len_item_name_error(value)
         self._name = value
 
     def discount_price(self):
+        """
+        Функция расчета скидок на товар
+        :return:
+        """
 
         try:
-
             x = self.item_price/100*Item.item_discount
             x = self.item_price-x
-
             return f'С учетом скидки, стоимость {self.item_name.lower()} составляет: {x} {self.currency}'
 
-        except KeyError:
+        except Exception:
             return f'Ошибка ввода информации'
 
     def total_price(self):
+        """
+        Функция вывода общей стоимости товара на складе
+        :return: Выводит стоимость товара на складе
+        """
         try:
             return f"Общая стоимость {self.item_name} на складе: {float(self.item_price * self.item_value)} {self.currency}"
-        except KeyError:
+
+        except Exception:
             return f'Ошибка ввода информации'
 
-    def len_item_name_check(self, name):
-        return len(name) > 10
+    def len_item_name_check(self, name, max_len=10):
+        """
+        Функция проверки на максимальную длинну названия товара
+        :param name: Название товара
+        :param max_len: Максимальная длинна
+        :return: Проверка на превышение максимальной длинны
+        """
+        return len(name) > max_len
 
-    def len_item_name_error(self):
-        raise Exception(f'Длинна названия товара не должна превышать 10 символов!')
+    def len_item_name_error(self, name):
+        """
+        Функция ошибки
+        :return: Возвращает ошибку
+        """
+        raise Exception(f'Длинна названия товара {name} не должна превышать 10 символов!')
 
 class CsvItems:
 
     all_items = []
 
     def __init__(self):
+        """
+        self.name - Хранит названия товаров
+        self.price - Хранит стоимость товара за 1 единицу
+        self.value - Хранит количество товара на складе
+        """
+        self.name = []
+        self.price = []
+        self.value = []
+
         with open('items.csv', newline='') as csv_items:
             csv_reader = csv.reader(csv_items)
             for items in csv_reader:
-                self.name=items[0]
-                self.price=items[1]
-                self.value=items[2]
-                self.all_items.append(f'Название: {self.name}, цена: {self.price}, количество: {self.value}')
+                #Пропускаем первую строку
+                if 'name' in items:
+                    continue
+                self.name.append(items[0])
+                self.price.append(items[1])
+                self.value.append(items[2])
+                self.all_items.append(f'Название: {items[0]}, цена: {items[1]}, количество: {items[2]}')
 
-print(len(CsvItems().all_items)-1)
+    def is_integer(self, numeric):
+        return isinstance(numeric, int)
+
+if __name__ == '__main__':
+
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфонyyytytyy'
+    print(item.name)
+
+    #item.name = 'СуперСмартфон'
+
+    csv_items = CsvItems()
+
+    print(len(csv_items.all_items))
+    print(csv_items.name[0])
+    print(csv_items.is_integer(5))
+    print(csv_items.is_integer(5.0))
 
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,10 @@ def test_main():
     assert main.Item('Ноутбук', 10000, 2, 'руб').total_price() == 'Общая стоимость Ноутбук на складе: 20000.0 руб'
     assert main.Item('Ноутбук', 'Test', 2, 'руб').discount_price() == 'Ошибка ввода информации'
     assert main.Item('Ноутбук', 'Test', 2, 'руб').total_price() == 'Ошибка ввода информации'
+    assert main.CsvItems().all_items[0] == 'Название: Смартфон, цена: 100, количество: 1'
+    assert main.CsvItems().is_integer(5) == True
+    assert main.CsvItems().is_integer(5.0) == False
+
+@pytest.mark.xfail
+def failed_test_main():
+    assert main.Item('СуперНоутбук123', 10000, 2, 'руб').total_price() == Exception
